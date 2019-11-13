0ed7f2e930f6085d72a1df61b33bc35a90a84271
iariiale <aleiarak@gmail.com>
2019-11-13 16:59:03 -0300

    Arreglo de errores en la api

--- src/main/java/Frontend/Handler.kt
+++ src/main/java/Frontend/Handler.kt
@@ -4,6 +4,6 @@ import Backend.Producto
 
 open class Handler(val code: Int, val type: String, open val message: String)
 class ClienteView(val nombre: String, val apellido: String, val domicilio: String, val contacto: Int, val cuit: String, val esConsumidorFinal: Boolean, val esResponsableInscripto: Boolean)
-data class ProductoView(val idCodigo: Int, val descripcion: String, val aplicaDescuento: Boolean, val aplicaPromo: Boolean, val precioCompra: Double, val precioVenta: Double) {
-    constructor(producto: Producto) : this(producto.idCodigo, producto.descripcion, producto.aplicaDescuento, producto.aplicaPromo, producto.precioCompra, producto.precioVenta)
+data class ProductoView(val idCodigo: Int, val descripcion: String, val precioCompra: Double, val precioVenta: Double) {
+    constructor(producto: Producto) : this(producto.idCodigo, producto.descripcion, producto.precioCompra, producto.precioVenta)
 }
