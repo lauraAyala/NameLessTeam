38f86c6739cd839b6322a2c1ec4304f8f7e4ef04
FerRodriguez <ferodriguez@adistec.com>
2019-11-18 17:56:53 -0300

    Título del menú principal

--- ferreteria/src/componente/Home.js
+++ ferreteria/src/componente/Home.js
@@ -13,8 +13,13 @@ class Home extends React.Component {
     render() {
         return (
             <div className={"Home"}>
-
-
+                <div className="container" >
+                <div className="title">
+                    <h2>Ferreteria<br/></h2>
+                    <h2>Bacco<br/></h2>
+                    {/*<Link target="_blank" to={{pathname: "/RegistrarCliente"}} className="white-mode">Registrarme</Link>*/}
+                </div>
+            </div>
             </div>
 
         );
